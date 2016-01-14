10f16f66ec86fad0f926c51549b4242297d9b17c
Kaguedes <tiagoasc.69@hotmail.com>
2016-01-14 02:04:16 +0000

    pda part 2

--- GeometryFriendsAgents/IAJ/ProblemDectection/ProblemDetectionAlgorithm.cs
+++ GeometryFriendsAgents/IAJ/ProblemDectection/ProblemDetectionAlgorithm.cs
@@ -34,15 +34,51 @@ namespace GeometryFriendsAgents.ProblemDectection
             foreach (Collectible c in WM.CollectibleList)
             {
                 Point pd = new Point(WM, c.xPos, c.yPos);
-                OpenPoints.Add(pd);
+                this.OpenPoints.Add(pd);
             }
         }
 
         public void GenerateConnections()
         {
+            while(OpenPoints.Count != 0)
+            {
+                /*Point op = OpenPoints.getBestAndRemove();
+                Point nop = OpenPoints.getBest();
 
-            throw new NotImplementedException();
+                //Right
+                if (op.yPos == nop.yPos)
+                {
+                    //slide
+                    if ((op.xPos - nop.xPos) <= 1 * this.WM.Matrix.WORLD_UNIT_SIZE)
+                    {
+                        op.addConnection(new Connection(WM, op, nop));
+                        nop.addConnection(new Connection(WM, nop, op));
+                    }
+                    //fall slide
+                    else if ((op.xPos - nop.xPos) <= 3*this.WM.Matrix.WORLD_UNIT_SIZE)
+                    {
+                        op.addConnection(new Connection(WM, op, nop));
+                        nop.addConnection(new Connection(WM, nop, op));
+                    }
+                }
+
+                //Down
+                Point pf = this.WM.Matrix.GenerateNewPoint(op);
+
+                //stair or gem TODO
+                if((op.yPos - pf.yPos) <= 2 * this.WM.Matrix.WORLD_UNIT_SIZE)
+                {
+                    op.addConnection(new Connection(WM, op, pf));
+                    pf.addConnection(new Connection(WM, pf, op));
+                }
+                //fall
+                else
+                {
+                    op.addConnection(new Connection(WM, op, pf));
+                }
+                this.OpenPoints.Add(pf);*/
+            }
         }
-
+        
     }
 }
