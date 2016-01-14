1f374fc2ba60ffbe6c37f76ea9679517bab2d9ac
Kaguedes <tiagoasc.69@hotmail.com>
2016-01-14 00:04:46 +0000

    pda first part

--- GeometryFriendsAgents/IAJ/ProblemDectection/ProblemDetectionAlgorithm.cs
+++ GeometryFriendsAgents/IAJ/ProblemDectection/ProblemDetectionAlgorithm.cs
@@ -18,6 +18,33 @@ namespace GeometryFriendsAgents.ProblemDectection
         public void GeneratePoints()
         {
 
+            foreach(Platform p in WM.PlatformList)
+            {
+                //add slide
+                Point pl = new Point(WM, p.xPos - (p.Width/2) , p.yPos + (p.Height/2));
+                Point pr = new Point(WM, p.xPos + (p.Width / 2), p.yPos + (p.Height / 2));
+            }
+            /* if(!(WM.Matrix.check(pl,0))){
+                var temp = M.getNextPos(pl, Left);
+                if(pl != temp){
+                   OpenPoints.Add(pl);
+                }
+            }
+
+            if(!(WM.Matrix.check(pr,0))){
+                var temp = M.getNextPos(pr, Right);
+                if(pr != temp){
+                   OpenPoints.Add(pr);
+                }
+            }
+            */
+
+            foreach(Collectible c in WM.CollectibleList)
+            {
+                Point pd = new Point(WM, c.xPos, c.yPos);
+                OpenPoints.Add(pd);
+            }
+
             throw new NotImplementedException();
         }
 
